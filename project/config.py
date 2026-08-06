6987c99f108b08f57319cf880cd6eb3dff81ad61
eryeni639-web <eryeni639@gmail.com>
2026-08-06 13:05:58 +0700

    Delete project directory

--- project/config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-OTP_API_KEY = os.getenv("OTPINSTAN_API_KEY")
-WEBHOOK_SECRET = os.getenv("WEBHOOK_SECRET")
